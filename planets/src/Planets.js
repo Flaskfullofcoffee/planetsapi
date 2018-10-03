7c6bcb812d081883118facedcf2efc34157c47d6
Zac <zdobbs3@gmail.com>
2018-10-03 11:58:56 -0700

    setting up foundations

---
+++ planets/src/Planets.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Planets extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+export default Planets;
--- planets/src/index.js
+++ planets/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Planets from './Planets.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Planets />, document.getElementById('root'));
 registerServiceWorker();
